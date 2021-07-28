e1d0b38846ec8d7c4d5e1cba186ed903e569c8e8
Kitatine <37077805+Kitatine@users.noreply.github.com>
2021-07-28 18:55:18 +0800

    Delete ui/node_modules directory

--- ui/node_modules/@cds/core/checkbox/index.js
+++
@@ -1,2 +0,0 @@
-export{CdsCheckbox}from"./checkbox.element.js";export{CdsCheckboxGroup}from"./checkbox-group.element.js";import"@cds/core/forms";
-//# sourceMappingURL=index.js.map
--- ui/node_modules/@cds/core/search/search.global.scss.js
+++
@@ -1,2 +0,0 @@
-import{css as e}from"lit";var t=e`cds-search input[type=search]::-webkit-search-cancel-button{display:none}`;export default t;
-//# sourceMappingURL=search.global.scss.js.map
